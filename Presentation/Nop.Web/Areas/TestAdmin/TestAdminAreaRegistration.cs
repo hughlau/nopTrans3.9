a839458536e552ee71c302c5885f16e22db03961
hugh lau <hughlaunet@gmail.com>
2018-07-05 14:05:18 +0800

    all commit

---
+++ Presentation/Nop.Web/Areas/TestAdmin/TestAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Nop.Web.Areas.TestAdmin
+{
+    public class TestAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TestAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TestAdmin_default",
+                "TestAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Presentation/Nop.Web/Areas/TestShow/TestShowAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Nop.Web.Areas.TestShow
+{
+    public class TestShowAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TestShow";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TestShow_default",
+                "TestShow/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
